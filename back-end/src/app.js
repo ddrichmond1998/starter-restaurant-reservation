69632760a484df567a9501b1f7a5252728e53111
ddrichmond1998 <drichmondhealthexec@gmail.com>
2022-01-31 19:13:16 -0600

    Commit Changed

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,7 +1,9 @@
 const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
+
 const express = require("express");
 const cors = require("cors");
+
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
 const reservationsRouter = require("./reservations/reservations.router");
