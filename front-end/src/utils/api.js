50a54dcac1287f47109284e70d1e7a1a15a7b5d3
ddrichmond1998 <drichmondhealthexec@gmail.com>
2022-02-02 21:06:26 -0600

    attempts

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,7 +6,7 @@
  import formatReservationDate from "./format-reservation-date";
  import formatReservationTime from "./format-reservation-time";
  
- const API_BASE_URL = "https://restaurant-reser-backend.herokuapp.com/"
+ const API_BASE_URL = "https://heroku-restrez.herokuapp.com"
  //process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
  
